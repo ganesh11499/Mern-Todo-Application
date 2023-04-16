409ea86d6d8a99cb14efc74b07c661f2b1fefb1f
Ganesh <ganeshramesh11499@gmail.com>
2023-04-16 18:46:02 +0530

    env change

--- index.js
+++ index.js
@@ -1,30 +1,27 @@
 import express from "express";
 
-import cors from 'cors'
+import cors from "cors";
 
 import mongoose from "mongoose";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 dotenv.config();
-import Routes from './route/route.js'
+import Routes from "./route/route.js";
 
-const DB = process.env.DATABASE.replace(
-  "<PASSWORD>",
-  process.env.DATABASE_PASSWORD
-);
+console.log(process.env.DATABASE);
+const DB = process.env.DATABASE
 mongoose.connect(DB).then((con) => console.log("DB connected"));
-
-const PORT = process.env.PORT || 3000
+console.log(DB)
+const PORT = process.env.PORT || 3000;
 
 const app = express();
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(cors());
 
-app.use('/', Routes)
-
+app.use("/", Routes);
 
 app.listen(PORT, () => {
-    console.log(`server running on ${PORT}`)
-})
+  console.log(`server running on ${PORT}`);
+});
